943185f972b441e13f26ef8d9e78f1a0d0587b2a
Kokemusi <74450392+Kokemusi@users.noreply.github.com>
2020-11-15 00:47:49 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,53 +1,90 @@
-from flask import Flask, request, abort
-import os
 
+import os
+import sys
+import wsgiref.simple_server
+from argparse import ArgumentParser
+
+from builtins import bytes
 from linebot import (
-    LineBotApi, WebhookHandler
+    LineBotApi, WebhookParser
 )
 from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
+    MessageEvent, TextMessage, TextSendMessage
 )
+from linebot.utils import PY3
 
-app = Flask(__name__)
+# get channel_secret and channel_access_token from your environment variable
+channel_secret = os.getenv('a2fc5a0a1e8b9172eb03deb54b87952c', None)
+channel_access_token = os.getenv('3/ejprz9CBzClM1nXMfFcSMVUHIiGoz+Uj3LqqqoPXmIJQdtNFLDemvT/bd0kgLVUIVtQSXmieW2yUabTnXx+IOuoj6UiH11eeMEmR1alA+gnDzYy42GSJAltcRPofgKu/VKtHgkGPRhE+eC19yERQdB04t89/1O/w1cDnyilFU=', None)
+if channel_secret is None:
+    print('Specify LINE_CHANNEL_SECRET as environment variable.')
+    sys.exit(1)
+if channel_access_token is None:
+    print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
+    sys.exit(1)
 
-#環境変数取得
-YOUR_CHANNEL_SECRET = os.getenv("a2fc5a0a1e8b9172eb03deb54b87952c")
-YOUR_CHANNEL_ACCESS_TOKEN = os.getenv("3/ejprz9CBzClM1nXMfFcSMVUHIiGoz+Uj3LqqqoPXmIJQdtNFLDemvT/bd0kgLVUIVtQSXmieW2yUabTnXx+IOuoj6UiH11eeMEmR1alA+gnDzYy42GSJAltcRPofgKu/VKtHgkGPRhE+eC19yERQdB04t89/1O/w1cDnyilFU=")
+line_bot_api = LineBotApi(channel_access_token)
+parser = WebhookParser(channel_secret)
 
-line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
-handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
-@app.route("/")
-def hello_world():
-    return "hello world!"
+def application(environ, start_response):
+    # check request path
+    if environ['PATH_INFO'] != '/callback':
+        start_response('404 Not Found', [])
+        return create_body('Not Found')
 
-@app.route("/callback", methods=['POST'])
-def callback():
+    # check request method
+    if environ['REQUEST_METHOD'] != 'POST':
+        start_response('405 Method Not Allowed', [])
+        return create_body('Method Not Allowed')
+
     # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
+    signature = environ['HTTP_X_LINE_SIGNATURE']
 
     # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
+    wsgi_input = environ['wsgi.input']
+    content_length = int(environ['CONTENT_LENGTH'])
+    body = wsgi_input.read(content_length).decode('utf-8')
 
-    # handle webhook body
+    # parse webhook body
     try:
-        handler.handle(body, signature)
+        events = parser.parse(body, signature)
     except InvalidSignatureError:
-        abort(400)
+        start_response('400 Bad Request', [])
+        return create_body('Bad Request')
 
-    return 'OK'
+    # if event is MessageEvent and message is TextMessage, then echo text
+    for event in events:
+        if not isinstance(event, MessageEvent):
+            continue
+        if not isinstance(event.message, TextMessage):
+            continue
 
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=event.message.text)
+        )
 
-if __name__ == "__main__":
-#    app.run()
-    port = int(os.getenv("PORT"))
-    app.run(host="0.0.0.0", port=port)
+    start_response('200 OK', [])
+    return create_body('OK')
+
+
+def create_body(text):
+    if PY3:
+        return [bytes(text, 'utf-8')]
+    else:
+        return text
+
+
+if __name__ == '__main__':
+    arg_parser = ArgumentParser(
+        usage='Usage: python ' + __file__ + ' [--port <port>] [--help]'
+    )
+    arg_parser.add_argument('-p', '--port', type=int, default=8000, help='port')
+    options = arg_parser.parse_args()
+
+    httpd = wsgiref.simple_server.make_server('', options.port, application)
+    httpd.serve_forever()
